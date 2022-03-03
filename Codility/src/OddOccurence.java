c3dadb53189942df5105c3234c53b00d6840454a
itsmeaneesh1 <javaglitz@gmail.com>
2022-03-03 09:35:32 +0530

    ds

--- Codility/src/OddOccurence.java
+++ Codility/src/OddOccurence.java
@@ -5,7 +5,7 @@ public class OddOccurence {
 	public int solution(int[] A) {
 		int output=0;
 	    HashMap <Integer, Integer> occurrencesMap = new HashMap<Integer, Integer>();
-	   
+	      
 	    for(int i=0; i<A.length; i++) {
 	      if(occurrencesMap.containsKey(A[i])) {
 	        occurrencesMap.put(A[i], occurrencesMap.get(A[i])+1); //increment occurrence counter and store it
